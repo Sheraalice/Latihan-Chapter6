b37eec462c23b0e5c66e5e259ba02b817e6e9bbc
Shera Alice <sheraalice@students.amikom.ac.id>
2023-11-03 13:17:00 +0700

    latihandeployment

--- app.js
+++ app.js
@@ -9,5 +9,6 @@ const userRoutes = require('./routes/auth.routes');
 const profileRoutes = require('./routes/profile.routes');
 app.use('/api/v1/auth', userRoutes);
 app.use('/api/v1/profile', profileRoutes);
+app.get('/', (req,res) => res.json({message: 'Welcome'}));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
